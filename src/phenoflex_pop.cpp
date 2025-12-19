55e6ad782d9a13ef25dfceae2bc31f6d73442c4f
larscaspersen <lars.caspersen@uni-bonn.de>
2025-12-19 23:59:00 +0100

    improved readability

--- src/phenoflex_pop.cpp
+++ src/phenoflex_pop.cpp
@@ -5,6 +5,7 @@ using namespace Rcpp;
 
 const double Pi = 3.14159265358979323846;
 
+//GDH model
 inline double P1z(const double T, const double Tu, const double Tb, const double Tc) {
   if(T >= Tb && T <= Tu) {
     return (1./2. * (1 + cos(Pi + Pi * (T - Tb)/(Tu - Tb))) );
@@ -15,35 +16,19 @@ inline double P1z(const double T, const double Tu, const double Tb, const double
   return (0.);
 }
 
+//alternative heat model
 inline double P2z(const double T, const double Tu, const double Delta) {
   return( exp(-((T - Tu)/2./Delta)*((T - Tu)/2./Delta)) );
 }
 
-// [[Rcpp::export]]
+//share of conversion labile chill to heat-stable chill 
+//also controls share of effective heat accumulation depending on chill
 inline double PFcn(const double T, const double Tf, const double slope) {
   const double x = slope*Tf*(T-Tf)/T;
   if(x >= 17) return(1);
   else if(x <= -20) return(0);
   const double sr = exp(x);
   return( sr/(1+sr) );
-}
-
-// draw values from normal distribution
-std::vector<double> gen_pop(const double mean, const double sd, const int n, const int seed = 12345) {   
-  std::mt19937 gen(seed); // generator
-
-    // Normal distribution with given mean and sd 
-  std::normal_distribution<> dist(mean, sd);
-  
-  // Vector to hold results 
-  std::vector<double> values; 
-  values.reserve(n); // reserve memory for efficiency
-  
-  // Draw n samples 
-  for (int i = 0; i < n; ++i) { 
-    values.push_back(dist(gen));
-    }
-  return values;
 }
 
 List PhenoFlex(NumericVector temp,
@@ -85,35 +70,85 @@ List PhenoFlex(NumericVector temp,
      _Tb += 273.;
    }
    int bloomindex = 0;
+   
+   //iterate over time steps
    for(int i = 0; i < N-1; i++) {
      double ti = temp[i];
      if(deg_celsius) ti += 273.;
+     
+     //chill increment and destruction
      xs[i] = A0/A1 * exp(-(E0-E1)/ti);
      const double k1 = A1*exp(-E1/ti);
+     
+     //update pools
      x[i+1] = xs[i] - (xs[i] - x[i])*exp(-k1*(times[i+1]-times[i]));
      y[i+1] = y[i];
+     
+     //calculate potential heat increment
      if(Imodel == 0) {
        z[i+1] = z[i] + P1z(ti, _Tu, _Tb, _Tc) * PFcn(y[i], yc, s1)*(times[i+1]-times[i]);
      }
      else {
        z[i+1] = z[i] + P2z(ti, _Tu, Delta) * PFcn(y[i], yc, s1)*(times[i+1]-times[i]);
      }
+     
+     //convert labile to stable chill
      if(x[i+1] >= 1.) {
        double delta = PFcn(ti, _Tf, slope) * x[i+1];
        y[i+1] += delta;
        x[i+1] -= delta;
      }
+     
+     //detect if heat requirement is met
      if(z[i+1] >= zc) {
        // i+2 for Fortran index convention in R
        bloomindex = i+2;
        if(stopatzc) break;
      }
+     
+     //return output
    }
    if(basic_output) {
      return List::create(Named("bloomindex") = bloomindex);
    }
    return List::create(Named("x") = x, Named("y") = y, Named("z")=z, Named("xs")=xs, Named("bloomindex") = bloomindex);
  }
+
+List run_forcing_experiment(NumericVector y, 
+                            NumericVector z,
+                              const double yc, 
+                              const double zc,
+                              const double s1, 
+                              const int timestep,
+                              const int max_days_forcing,
+                              const double forcing_increment,
+                              const int placeholder_fail = -999){
+  
+  const int hours = 24; //numbers of hours forcing per day
+  double y_i = y[timestep]; // get amount of chill at forcing experiment 
+  const double share_effective_heat_i = PFcn(y_i, yc, s1);
+  int bloom_met = placeholder_fail; //only overwrite if bloom is met
+
+  //amount of forcing already accumulated before cutting
+  double base_force = z[timestep];
+  
+  //iterate through one experiment, each time step add forcing
+  for (int k = 0; k < max_days_forcing*hours; k++) {
+    //assume constant temperatures, each increment is same 
+    double exp_force = base_force +  (forcing_increment* share_effective_heat_i * k);
+    
+    //check if zc is met
+    if (exp_force >= zc){
+      bloom_met = k;
+      break;
+    } 
+  } //end loop days in forcing experiments
+  
+  return(List::create(Named("timestep_bloom") = bloom_met,
+                      Named("share_effective_heat") = share_effective_heat_i));
+}
+
+
 // [[Rcpp::export]]
 List PhenoFlex_pop(NumericVector temp,
                    NumericVector times,
@@ -121,7 +156,7 @@ List PhenoFlex_pop(NumericVector temp,
                    const Rcpp::NumericVector zc,
                    const int max_days_forcing = 50,
                    Nullable<NumericVector> i_cut = R_NilValue ,
-                   const int force_temp = 23,
+                   const int forcing_temperature = 23,
                    const double s1=0.5,
                    const double E0=3372.8,
                    const double E1=9900.3,
@@ -139,61 +174,46 @@ List PhenoFlex_pop(NumericVector temp,
                    bool deg_celsius = true,
                    bool basic_output = true){
   
-  // draw population of yc and zc
-  //std::vector<double> yc_pop = gen_pop(yc, yc_sd, n, seed);
-  //std::vector<double> zc_pop = gen_pop(zc, zc_sd, n, seed+1);
-  
-  bool return_force_exp = false;
   //unwrap the vector
   NumericVector i_cut_vec(i_cut.get());
   
-  //vector that keeps track how many time steps after cutting bloom was
-  //reached in experiment
-  //will be saved later to the exp_bloom_pop list
-  //-999 is placeholder as failure to reach zc
-  const std::vector<int> exp_bloom_base(i_cut_vec.size(), -999);
+  const int N = yc.size();
+  const int time_steps = times.size();
+  const int n_experiments = i_cut_vec.size();
+  const int placeholder_fail = -999;
+  double forcing_increment = 0;
+  //double z_effec = 0; //share of potential heat that becomes effective (forcing experiment)
+  bool return_force_exp = false;
   
+  //store forcing experiment results (time steps needed to reach zc)
+  NumericVector exp_bloom_base(i_cut_vec.size(), placeholder_fail);
   
-  double force_inc = 0;
-  
-  double z_effec = 0; //variable for forcing experiment, how much heat increment actually accumulated
-  
-  // override basic_output depending on i_cut 
+  // if forcing experiment days are supplied (i_cut)
   if (i_cut.isNull() == false) { 
     return_force_exp = true; 
 
     //calculate the forcing potential for each day of experiment
     //assume constant conditions
-    force_inc = P1z(force_temp, Tu, Tb, Tc);
-    
+    forcing_increment = P1z(forcing_temperature, Tu, Tb, Tc);
   }
   
-  //in case both sds are 1 and we effectively only sample one value
-  //run the calculations only once
-  int n_run = yc.size();
+  //where results will be saved
+  Rcpp::NumericVector bloom_ind(N); //vector with time step of bloom (for each member of population)
+  NumericMatrix x_pop(time_steps, N); //time step per row, population member per column
+  NumericMatrix y_pop(time_steps, N); //time step per row, population member per column
+  NumericMatrix z_pop(time_steps, N); //time step per row, population member per column
+  NumericMatrix exp_bloom_pop(n_experiments, N); //experiment: time step after cutting zc reached
 
-  Rcpp::NumericVector bloom_ind(n_run);
-  NumericMatrix x_pop(times.size(), n_run);
-  NumericMatrix y_pop(times.size(), n_run);
-  NumericMatrix z_pop(times.size(), n_run);
-  NumericMatrix exp_bloom_pop(i_cut_vec.size(),n_run); //list with the forcing of the individual cutting days
-  //vector with as many positions as experiments
-  // entry says how many time steps after cutting the zc was reached
-  //-999 in case it was not met
-  
-  
   ////////////////////
   //DEBUGGING
-  
-  NumericMatrix z_effec_exp(i_cut_vec.size(), n_run);
-
+  NumericMatrix z_effec_exp(n_experiments, N); //share of potential heat that gets accumulated
   // END DEBUGGING
   ////////////////////
   
-
   //run phenoflex, return complex output
-  for (int i = 0; i < n_run; ++i) { 
+  for (int i = 0; i < N; ++i) { 
     
+    //basic bloom calculation
     List pheno_out = PhenoFlex(temp,
                           times,
                           yc[i],
@@ -216,68 +236,77 @@ List PhenoFlex_pop(NumericVector temp,
     bloom_ind[i] = pheno_out["bloomindex"];
     
     //reset the vector tracking the forcing experiment
-    std::vector<int> exp_bloom_i = exp_bloom_base;
-    Rcpp::NumericVector z_effec_vec(i_cut_vec.size());
+    NumericVector exp_bloom_i = exp_bloom_base;
+    Rcpp::NumericVector share_effective_heat(n_experiments);
     
     if (basic_output == false){
       //extract values from phenoflex list
-      NumericVector x_int = pheno_out["x"];
-      NumericVector y_int = pheno_out["y"];
-      NumericVector z_int = pheno_out["z"];
-
-      //save results to matrix
-      std::copy(x_int.begin(), x_int.end(), x_pop.begin() + i * times.size());
-      std::copy(y_int.begin(), y_int.end(), y_pop.begin() + i * times.size());
-      std::copy(z_int.begin(), z_int.end(), z_pop.begin() + i * times.size());
-      
-
+      x_pop(_, i) = as<NumericVector>(pheno_out["x"]);
+      y_pop(_, i) = as<NumericVector>(pheno_out["y"]);
+      z_pop(_, i) = as<NumericVector>(pheno_out["z"]);
     }
-    
     
     // do forcing experiment
     if(return_force_exp){
       
       //iterate over forcing experiment days
-      for (int j = 0; j<i_cut_vec.size(); j++) {
+      for (int j = 0; j<n_experiments; j++) {
         
-        NumericVector chill = y_pop.column(i); // extract the i-th NumericVector 
-        double val = chill[i_cut_vec[j]]; // get amount of chill at forcing experiment 
-        z_effec = PFcn(val, yc[i], s1); //calculate effectivity of forcing experiment
+        List forcing_out = run_forcing_experiment(y_pop.column(i),
+                                                  z_pop.column(i),
+                                                  yc[i],
+                                                  zc[i],
+                                                  s1,
+                                                  i_cut_vec[j],
+                                                  max_days_forcing,
+                                                  forcing_increment,
+                                                  placeholder_fail);
+        
+        //save results of experiment to vectors
+        share_effective_heat[j] = forcing_out["share_effective_heat"];
+        exp_bloom_i[j] = forcing_out["timestep_bloom"];
+        
+        
+        //NumericVector chill = y_pop.column(i); // extract the i-th NumericVector 
+        //double val = chill[i_cut_vec[j]]; // get amount of chill at forcing experiment 
+        //z_effec = PFcn(val, yc[i], s1); //calculate effectivity of forcing experiment
 
         ////////
         //DEBUG
-        z_effec_vec[j] = z_effec;
+        //share_effective_heat[j] = z_effec;
         ////////
         
-        NumericVector  z_vec = z_pop.column(i); //extract heat accumulated for that individual
+        //NumericVector  z_vec = z_pop.column(i); //extract heat accumulated for that individual
         
         //amount of forcing already accumulated before cutting
-        double base_force = z_vec[i_cut_vec[j]];
+        //double base_force = z_vec[i_cut_vec[j]];
         
 
         //timesteps within experiment
         //amount of forcing that was already accumulated + extra forcing from experiment * effectivity (controlled by y, yc, s1)
-        for (int k = 0; k < max_days_forcing*24; k++) {
-          double exp_force = base_force +  (force_inc* z_effec * k);
+        //for (int k = 0; k < max_days_forcing*hours; k++) {
+        //  double exp_force = base_force +  (forcing_increment* z_effec * k);
           
           //check if zc is met
-          if (exp_force >= zc[i]){
-            exp_bloom_i[j] = k;
-            break;
-          } 
+        //  if (exp_force >= zc[i]){
+        //    exp_bloom_i[j] = k;
+        //    break;
+        //  } 
 
-        } //end loop days in forcing experiments
+        //} //end loop days in forcing experiments
         
  
       } //end loop time steps forcing experiments
     } //end experiment
     
     //save result to matrix
-    std::copy(exp_bloom_i.begin(), exp_bloom_i.end(), exp_bloom_pop.begin() + i * i_cut_vec.size());
+    exp_bloom_pop(_, i) = as<NumericVector>(exp_bloom_i);
+    //std::copy(exp_bloom_i.begin(), exp_bloom_i.end(), exp_bloom_pop.begin() + i * n_experiments);
     
     ////////
     //DEBUG
-    std::copy(z_effec_vec.begin(), z_effec_vec.end(), z_effec_exp.begin() + i * i_cut_vec.size());
+    z_effec_exp(_, i) = as<NumericVector>(share_effective_heat);
+    //std::copy(share_effective_heat.begin(), share_effective_heat.end(), z_effec_exp.begin() + i * n_experiments);
     //z_effec_list[i] = z_effec_vec;
     ////////
     
@@ -292,5 +321,5 @@ List PhenoFlex_pop(NumericVector temp,
                       Named("z") = z_pop,
                       Named("exp") = exp_bloom_pop,
                       Named("z_effec") = z_effec_exp,
-                      Named("exp_force_inc") = force_inc));
+                      Named("exp_force_inc") = forcing_increment));
 }
